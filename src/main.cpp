0b9d111d443e4f8415c1e1113637afcfb48b0fd9
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-02-12 19:19:32 -0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define DIGITAL_PIN 3
+#define ANALOG_PIN A0
+
+void setup() {
+  pinMode(DIGITAL_PIN, INPUT);
+  pinMode(ANALOG_PIN, INPUT);
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print(" Microphone  Sensor");
+
+  lcd.setCursor(0, 3);
+  lcd.print("Value:");
+}
+
+void loop() {
+  lcd.setCursor(0, 2);
+
+  if (digitalRead(DIGITAL_PIN)) {
+    lcd.print("** SOUND DETECTED **");
+    delay(500);
+  } else {
+    lcd.print("                    ");
+  }
+
+  lcd.setCursor(7, 3);
+  lcd.print(analogRead(ANALOG_PIN));
+
+  delay(100);
+}
